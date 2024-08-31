f9e70f89a37546209f7975b47885ee6be2716e0e
Chris Mwanya <cmwanya@gmail.com>
2024-08-31 15:28:51 +0100

    chore: Add feed, techno, project, guestbook, and role-project API routes, controllers, services, and content types

---
+++ src/api/role-project/routes/role-project.ts
@@ -0,0 +1,7 @@
+/**
+ * role-project router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::role-project.role-project');
---
+++ src/api/role-project/services/role-project.ts
@@ -0,0 +1,7 @@
+/**
+ * role-project service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::role-project.role-project');
